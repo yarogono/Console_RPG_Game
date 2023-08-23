58f4b5912d86f3723fef28ffd6c5dd5e11396a56
yarogono <70641418+yarogono@users.noreply.github.com>
2023-08-23 16:16:55 +0900

    feat/상태보기 아이템 능력치 추가

--- Client/ConsoleUI/UI_PlayerStatus.cs
+++ Client/ConsoleUI/UI_PlayerStatus.cs
@@ -1,4 +1,5 @@
 ﻿using Client.Models;
+using Client.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +14,52 @@ namespace Client.ConsoleUI
         {
             Console.Clear();
 
-            string playerStatusStr = "상태보기\n" +
-                                            "캐릭터의 정보로 표시합니다.\n" +
-                                            "\n" +
-                                            $"Lv.{player.Level}\n" +
-                                            $"{player.Name}({player.CharacterClass})\n" +
-                                            $"공격력 :{player.Attack}\n" +
-                                            $"방어력 : {player.Defense}\n" +
-                                            $"체력 : {player.Hp}\n" +
-                                            $"Gold : {player.Gold} G\n" +
-                                            $"\n" +
-                                            $"0. 나가기\n" +
-                                            $"원하시는 행동을 입력해주세요.\n" +
-                                            $">> ";
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("상태보기\n" +
+                           "캐릭터의 정보로 표시합니다.\n" +
+                           "\r\n" +
+                          $"Lv.{player.Level}\n" +
+                          $"{player.Name}({player.CharacterClass})\n");
+
+            if (player.Inventory.EquipItem.Count >= 1)
+            {
+                int attack = 0;
+                int defense = 0;
+                foreach (Item item in player.Inventory.EquipItem)
+                {
+                    string[] statStr = item.Stat.Split();
+                    if (statStr.Contains("공격력"))
+                    {
+                        int statNum = int.Parse(statStr[1].Replace('+', ' '));
+                        attack += statNum;
+                    }
+                    else if (statStr.Contains("방어력"))
+                    {
+                        int statNum = int.Parse(statStr[1].Replace('+', ' '));
+                        defense += statNum;
+                    }
+                }
+                
+                sb.Append($"공격력 :{player.Attack} (+{attack})\n" +
+                              $"방어력 : {player.Defense} (+{defense})\n");
+            }
+            else
+            {
+                sb.Append($"공격력 :{player.Attack}\n" +
+                               $"방어력 : {player.Defense}\n");
+            }
 
 
-            Console.Write(playerStatusStr);
+            sb.Append($"체력 : {player.Hp}\n" +
+                           $"Gold : {player.Gold} G\n" +
+                           $"\r\n" +
+                           $"0. 나가기\n" +
+                           $"원하시는 행동을 입력해주세요.\n" +
+                           $">> ");
+
+
+            Console.Write(sb.ToString());
         }
     }
 }
